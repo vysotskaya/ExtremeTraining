37c6c2f6d1e0dfd632a5e77ec21360acdfa54d54
ekaterinatsmyg <ekaterina.tsmyg@gmail.com>
2016-02-19 13:13:08 +0300

    Add comment repository

---
+++ DAL.Interface/Repositories/ICommentRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL.Interface.DalEntities;
+
+namespace DAL.Interface.Repositories
+{
+    public interface ICommentRepository: IRepository<DalComment>
+    {
+    }
+}
